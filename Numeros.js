40c5c96912b6b0e16deba8667ebb12911811ddc6
JcalleA <kalle-1413@hotmail.com>
2022-10-04 16:57:18 -0500

    ok

---
+++ Numeros.js
@@ -0,0 +1,17 @@
+
+let alturaCm = 172;
+
+let alturaMts = alturaCm/100;
+console.log(alturaMts);
+
+let pesoKg = 90.5;
+
+let alturaRoundMts2 = Math.round(alturaMts);
+console.log(alturaRoundMts2);
+
+let pesoRoundKg = Math.floor(pesoKg);
+console.log(pesoRoundKg);
+
+let maxValue = Number.MAX_VALUE*1==Number.MAX_VALUE;
+console.log(maxValue);
+
