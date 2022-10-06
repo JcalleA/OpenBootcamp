676b01c37d8d412e5b6f6d07b3974f2723bd1744
JcalleA <kalle-1413@hotmail.com>
2022-10-06 14:40:00 -0500

    ok

---
+++ Java Basico/src/Bucle.java
@@ -0,0 +1,18 @@
+public class Bucle {
+
+    static String[] lista={"hola","como","estas"};
+    static String lista2="";
+
+    public static void main(String[] args) throws Exception {
+
+        
+
+        for(String a : lista) {
+            lista2+=" "+a;
+            System.out.println(lista2);
+            
+
+    }
+    
+}
+}
